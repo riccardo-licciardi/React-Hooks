9a3da78bcca477fd8c02e25583d2bfe1a76b8d54
rlicciardi <riccardo.licciardi@gmail.com>
2022-05-17 12:34:58 +0200

    lezione1 - useState Base

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Inizio from "./lezioni/5-useRef/inizio/useref-example";
+import Inizio from "./lezioni/1-useState/inizio/2-useState-basi";
 
 function App() {
   return (
--- src/lezioni/1-useState/inizio/2-useState-basi.js
+++ src/lezioni/1-useState/inizio/2-useState-basi.js
@@ -1,13 +1,14 @@
 import React from "react";
+import { useState } from "react/cjs/react.production.min";
 // Gli Hooks devono essere usasti con use davanti
 // componenti devono avere la prima lettere Maiuscola
 // Gli Hook devono essere invocati all'interno del corpo/funzione del componente
 // Gli Hook non possono essere utilizzati in maniera condizionale
 
 const UsoBase = () => {
-  //   const value = useState()[0];
-  //   const handler = useState()[1];
-  //   console.log(value, handler);
+  const value = useState()[0];
+  const handler = useState()[1];
+  console.log(value, handler);
 
   return (
     <>
